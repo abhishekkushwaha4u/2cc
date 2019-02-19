b8f6be1e87a2b4466d76fbe84ecb2ffd90347792
abhishekkushwaha4u <thecrazycoderabhi@gmail.com>
2019-02-19 21:10:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const bp = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const bcrypt = require("bcryptjs")
+
+// require all model funcs
+const {
+    CreateUser,
+    ReadUser,
+    DeleteUser,
+    UpdateUser
+} = require("./schema") 
+
+// handle post requests
+app.use(bp.json());
+app.use(bp.urlencoded({extended:false}));
+
+/**
+ * 
+ * Connect to DB-
+ * 1. require mongoose ( ORM )
+ * 2. connect to DB
+ * 3. create a schema
+ * 4. create CRUD
+ * 5. export functions
+ */
+
+ // 1. Connect to DB
+mongoose.connect("mongodb://admin123:admin123@ds119070.mlab.com:19070/2cc", {useNewUrlParser:true})
+
+// 2. Check if connected
+mongoose.connection.once("connected", ()=>{
+    console.log("Connected to DB")
+}).on("error", ()=>{
+    console.log("Error connecting to DB")
+})
+
+// test route
+app.get("/", (req,res)=>{
+    res.json({
+        message:"hello world"
+    })
+})
+
+// handle user creation
+app.post("/register", async (req,res)=>{
+    // call DB func
+    res.json( await CreateUser(req.body) )
+})
+
+
+// handle user read
+app.post("/read", async (req,res)=>{
+    // call DB func
+    res.json( await ReadUser(req.body) )
+})
+
+
+
+// handle user delete
+app.post("/delete", async (req,res)=>{
+    // call DB func
+    res.json( await DeleteUser(req.body) )
+})
+
+
+// handle user update
+app.post("/update", async (req,res)=>{
+    // call DB func
+    res.json( await UpdateUser({
+        username:req.body.username
+    }, req.body.update) )
+})
+
+/**
+ * 1. exists or not?
+ * 2. if yes, hash password and compare
+ */
+app.post("/login", async (req,res)=>{
+    users = await ReadUser({username:req.body.username})
+    if(users.length < 1) {
+        return res.json({err:"No user"})
+    } 
+
+    if(users[0].password == bcrypt.hashSync(req.body.password, 10)) {
+        return res.json({messaage:"Logged in !"})
+    }
+    return res.json({err:"wrong password"})
+
+})
+
+// listen
+app.listen(3000, ()=>console.log("Listening..."))
---
+++ schema.js
@@ -0,0 +1,76 @@
+/**
+ * 1. create schema
+ * 2. create model
+ * 3. using model, create CRUD (create read update delete) functions
+ * 4. export em
+ */
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
+
+let UserSchema = new mongoose.Schema({
+    username:String,
+    password:String
+})
+
+let userModel = mongoose.model("user", UserSchema)
+
+
+/**
+ * 1. check if user already exists or not?
+ * 2. if user DNE
+ *      2.1 hash user password
+ *      2.2 create
+ *    else throw error
+ * 
+ */
+module.exports.CreateUser = async (data) =>{
+    
+    // does user exist?
+    user = await userModel.find({"username":data.username})
+    if(user.length > 0){
+        return {err:"User already exists"}
+    }
+
+    // if not, hash password and save
+
+    pw = hash(data.password)
+
+    return await userModel.create({
+        username:data.username,
+        password:pw
+    })
+    // .then((user)=>{
+
+    // }).catch((err)=>{
+
+    // })
+} 
+
+
+/**
+ * query format-
+ * 
+ * {
+ *      "username":"angad"
+ * }
+ */
+module.exports.ReadUser = async (query)=>{
+    return await userModel.find(query)
+}
+
+module.exports.DeleteUser = async (query)=>{
+    return await userModel.findOneAndDelete(query)
+}
+
+
+
+
+
+module.exports.UpdateUser = async (user, update) =>{
+    return await userModel.findOneAndUpdate(user, update)
+} 
+
+let hash = (password)=>{
+    return bcrypt.hashSync(password, 10)
+}
